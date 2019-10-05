afbbd03fd19317c778c32ca02bc47ac085af0452
1_Hours <sharipov.yanepenb@gmail.com>
2019-10-05 15:47:54 +0700

    liner

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,19 +3,19 @@
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->get('/');
+// class ExampleTest extends TestCase
+// {
+//     /**
+//      * A basic test example.
+//      *
+//      * @return void
+//      */
+//     public function testExample()
+//     {
+//         $this->get('/');
 
-        $this->assertEquals(
-            $this->app->version(), $this->response->getContent()
-        );
-    }
-}
+//         $this->assertEquals(
+//             $this->app->version(), $this->response->getContent()
+//         );
+//     }
+// }
