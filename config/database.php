21a9e599142736ec1a7d5be70aa92d5ba2dce3bd
1_Hours <sharipov.yanepenb@gmail.com>
2019-12-06 14:47:09 +0700

    add pqsql config

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 return [
     //...
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
     //...
     'connections' => [
         'testing_sqlite' => [
@@ -10,6 +10,18 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'username'  => env('DB_TEST_USERNAME', '1h'),
             'password'  => env('DB_TEST_PASSWORD', '1h'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'   => '',
+        ],
+
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'      => env('DB_HOST', 'ec2-174-129-255-39.compute-1.amazonaws.com'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'd45kvutj881cij'),
+            'username'  => env('DB_USERNAME', 'woxwuwmassmidl'),
+            'password'  => env('DB_PASSWORD', '5940ed635ea30d5b2ebfb1b004c2c41333e84a8e84be76c9678d550d9d524f41'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'   => '',
