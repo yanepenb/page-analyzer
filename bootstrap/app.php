7ae5b754fac9eb5e7d666ce0456d432569058e40
1_Hours <sharipov.yanepenb@gmail.com>
2019-12-19 23:14:13 +0700

    debug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,7 @@ $app = new Laravel\Lumen\Application(
 if (env('APP_DEBUG')) {
     $app->configure('app');
     $app->configure('debugbar');
-    $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
+    //$app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
 }
 
 $app->withFacades();
