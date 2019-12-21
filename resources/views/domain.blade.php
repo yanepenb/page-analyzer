9fb9bbb2e0b394a6b8b2d12c4165b4f1a512beab
1_Hours <sharipov.yanepenb@gmail.com>
2019-12-21 09:55:14 +0700

    redact domains with domain

--- resources/views/domain.blade.php
+++ resources/views/domain.blade.php
@@ -5,11 +5,8 @@
                 <tr>
                 <th scope="col">#</th>
                 <th scope="col">Name</th>
-                <th scope="col">updated_at</th>
-                <th scope="col">created_at</th>
-                <th scope="col">content_length</th>
-                <th scope="col">h1</th>
                 <th scope="col">response_code</th>
+                <th scope="col">content_length</th>
                 <th scope="col">keywords</th>
                 <th scope="col">description</th>
                 </tr>
@@ -18,11 +15,8 @@
                 <tr>
                 <th scope="row">{{ $domain->id }}</th>
                 <td><a href="{{ $domain->name }}" target="_blank">{{ $domain->name }}</a></td>
-                <td>{{ $domain->updated_at }}</td>
-                <td>{{ $domain->created_at }}</td>
-                <td>{{ $domain->content_length }}</td>
-                <td>{{ $domain->h1 }}</td>
                 <td>{{ $domain->response_code }}</td>
+                <td>{{ $domain->content_length }}</td>
                 <td>{{ $domain->keywords }}</td>
                 <td>{{ $domain->description }}</td>
                 </tr>
--- resources/views/domains.blade.php
+++ resources/views/domains.blade.php
@@ -5,11 +5,11 @@
                 <tr>
                 <th scope="col">#</th>
                 <th scope="col">Name</th>
-                <th scope="col">updated_at</th>
-                <th scope="col">created_at</th>
+                <th scope="col">response_code</th>
                 <th scope="col">content_length</th>
                 <th scope="col">h1</th>
-                <th scope="col">response_code</th>
+                <th scope="col">updated_at</th>
+                <th scope="col">created_at</th>
                 </tr>
             </thead>
             @foreach ($domains as $domain)
@@ -17,11 +17,11 @@
                     <tr>
                     <th scope="row"><a href="/domains/{{ $domain->id }}">{{ $domain->id }}</a></th>
                     <td><a href="{{ $domain->name }}" target="_blank">{{ $domain->name }}</a></td>
-                    <td>{{ $domain->updated_at }}</td>
-                    <td>{{ $domain->created_at }}</td>
+                    <td>{{ $domain->response_code }}</td>
                     <td>{{ $domain->content_length }}</td>
                     <td>{{ $domain->h1 }}</td>
-                    <td>{{ $domain->response_code }}</td>
+                    <td>{{ $domain->updated_at }}</td>
+                    <td>{{ $domain->created_at }}</td>
                     </tr>
                 </tbody>
             @endforeach
